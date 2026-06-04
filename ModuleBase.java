0c061bb332bf02b3476e1b55c14f7ee38f26eae5
aininjeffery <aininjeffery@gmail.com>
2026-06-05 03:53:33 +0800

    Update ModuleBase.java

--- ModuleBase.java
+++ ModuleBase.java
@@ -2,20 +2,13 @@
 
 import javax.swing.*;
 
-public abstract class ModuleBase extends JFrame {
+public abstract class ModuleBase extends JFrame implements ModuleAction {
+
     protected String name;
     protected int score;
 
     public ModuleBase(String title, String name, int score) {
         this.name = name;
         this.score = score;
-
-        setTitle(title);
-        setSize(AppConfig.PHONE_WIDTH, AppConfig.PHONE_HEIGHT);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setResizable(false);
     }
-
-    public abstract void openModule();
 }
