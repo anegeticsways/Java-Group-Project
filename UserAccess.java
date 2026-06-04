9069534b93111e8b7dbaf05fc0e1911827751115
aininjeffery <aininjeffery@gmail.com>
2026-06-05 03:07:16 +0800

    Update UserAccess.java

--- UserAccess.java
+++ UserAccess.java
@@ -6,11 +6,10 @@ Description:
 1. Defines the interface for accessing user data in the eWaste education application
 */
 
-
 import java.util.ArrayList;
 
 public interface UserAccess {
-    ArrayList<User> getAllUsers(); // Method to retrieve all users from userdata.txt in UserFileAccess.java
-    void saveAllUsers(ArrayList<User> users); // Method to save all users to userdata.txt in UserFileAccess.java
-    void updateUserScore(String name, int newScore); // Method to update a specific user's score in userdata.txt in UserFileAccess.java
+    ArrayList<User> loadUsers();
+    void saveAllUsers(ArrayList<User> users);
+    void updateUserScore(String name, int score);
 }
