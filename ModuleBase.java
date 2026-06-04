50e041d9be9af6297e86a98c56a3cb296692ab6b
aininjeffery <aininjeffery@gmail.com>
2026-06-05 05:10:04 +0800

    Update ModuleBase.java

--- ModuleBase.java
+++ ModuleBase.java
@@ -1,13 +1,10 @@
 //Contributed by Ainin (102810)
 
-import javax.swing.*;
-
-public abstract class ModuleBase extends JFrame implements ModuleAction {
-
+public abstract class ModuleBase implements ModuleAction {
     protected String name;
     protected int score;
 
-    public ModuleBase(String title, String name, int score) {
+    public ModuleBase(String name, int score) {
         this.name = name;
         this.score = score;
     }
