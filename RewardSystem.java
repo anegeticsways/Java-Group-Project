d3832f97dfef2dfbb41c609e04a518ba751b9328
aininjeffery <aininjeffery@gmail.com>
2026-06-05 02:24:00 +0800

    Update RewardSystem.java

--- RewardSystem.java
+++ RewardSystem.java
@@ -7,13 +7,11 @@ Defines the criteria and thresholds for earning different rewards.
 */
 
 public interface RewardSystem {
-    
-    // Thresholds for earning badges based on score
+
     int BRONZE_THRESHOLD = 20;
     int SILVER_THRESHOLD = 50;
     int GOLD_THRESHOLD = 80;
 
-    // Methods that must be implemented by the GamificationEngine
     String determineBadge(int score);
     String getMotivationalMessage(int score);
     int calculatePointsToNextBadge(int score);
