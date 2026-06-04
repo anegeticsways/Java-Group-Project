f46349afd286df3b95a4137f12eada7aca3f33a5
anegeticsways <137865505+anegeticsways@users.noreply.github.com>
2026-06-04 23:51:38 +0800

    Refactor comments in Displayable interface
    Updated comments format to block comment style.

--- Displayable.java
+++ Displayable.java
@@ -1,7 +1,10 @@
-// Contributed by (Ailin Najwa)(102390)
-// Role: Member 2 - Content Specialist
-// Description:
-// This interface sets the standard for displaying and navigating learning pages.
+/* 
+Contributed by Ailin Najwa (102390)
+Role: Member 2 - Content Specialist
+
+Description:
+This interface sets the standard for displaying and navigating learning pages.
+*/
 
 public interface Displayable {
     void displayPage();
