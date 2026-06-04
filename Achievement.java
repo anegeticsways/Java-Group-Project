44ba06d6b799e43c7246ab1993697d26ae0e4417
aininjeffery <aininjeffery@gmail.com>
2026-06-04 18:53:31 +0800

    Update Achievement.java

--- Achievement.java
+++ Achievement.java
@@ -1,39 +1,41 @@
 /*
-Contributed by Andrean (103325)
+Created by: Andrean (103325)
+Tested by: Ainin (102810)
+
 Description:
-1. Contribute basic structure of Achievement.java only - call displayAchievements() method to display all users score in console
-2. User.java allows user to view scores for all users
-3. [NOT YET] It includes elements like badges/title earned
+1. Displays all users' scores.
+2. Sorts users from highest score to lowest score.
+3. Shows badge/title earned using GamificationEngine.
 */
 
-/*
-Thoughts and Comments:
-1. Can assign badges or title for user with top 10 highest marks
-2. Sort user data based on the highest marks
-*/
-
-
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Achievement {
 
-    //UNSORTED - Display all users score in Console
     public void displayAchievements() {
-        //Call all users method to load user data
         ArrayList<User> users = User.loadUsers();
-       
-        // Contributed by Afiq (103041)
         GamificationEngine engine = new GamificationEngine();
 
-        for (User u : users) {
-            // Get the badge for each user's score
-            String badge = engine.determineBadge(u.getScore()); 
-            
-            // Print the name, score, AND badge to the console
-            System.out.println(u.getName() + " - " + u.getScore() + " Points | Rank: " + badge);
+        users.sort((u1, u2) -> u2.getScore() - u1.getScore());
+
+        String scoreboard = "Score Board\n\n";
+
+        for (int i = 0; i < users.size(); i++) {
+            User u = users.get(i);
+            String badge = engine.determineBadge(u.getScore());
+
+            scoreboard += (i + 1) + ". " +
+                          u.getName() + " - " +
+                          u.getScore() + " Points | " +
+                          badge + "\n";
         }
-    }    
+
+        JOptionPane.showMessageDialog(
+            null,
+            scoreboard,
+            "Score Board",
+            JOptionPane.INFORMATION_MESSAGE
+        );
+    }
 }
