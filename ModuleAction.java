76657bae153c7f5a7533f952c99012fe7c332b90
anegeticsways <137865505+anegeticsways@users.noreply.github.com>
2026-06-05 14:05:59 +0800

    Update ModuleAction.java

--- ModuleAction.java
+++ ModuleAction.java
@@ -1,7 +1,4 @@
 /*
-Contributed by Ailin Najwa (102390)
-Role: Member 2 - Content Specialist
-
 Description:
 1. Interface for module actions, specifically to open the learning module.
 2. Implemented by LearningModule to allow it to be opened from the main menu or other parts of the application.
